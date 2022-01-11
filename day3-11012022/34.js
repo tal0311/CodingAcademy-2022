f2770f26a0e19fe2a66a153f0eb6fb327b8a89ef
tal0311 <tal.amit0311@gmail.com>
2022-01-11 22:06:47 +0200

    34.fix bug

--- day3-11012022/34.js
+++ day3-11012022/34.js
@@ -5,18 +5,18 @@ Implement a function named myIndexOf(str, searchStr) which receives two strings.
 The function returns the index of the second string within the first, or -1 if it wasn’t found (do not use the built-in indexOf()function… ).*/
 
 var str1 = 'first string' //return index of this
-var str2 = 'h'
+var str2 = 's'
 
 var res = myIndexOf(str1, str2)
 console.log('res:', res)
 
 function myIndexOf(str, searchStr) {
-  console.log(str, searchStr)
+  var index = 0
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < searchStr.length; j++) {
-      if (searchStr.charAt(j) === str.charAt(i)) {
-        return i
-      } else return -1
+      if (searchStr.charAt() === str.charAt(i)) return (index = i)
+      else index = -1
     }
   }
+  return index
 }
